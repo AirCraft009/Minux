00d0cdeb34b9be582430bbf474f8ec1ad043eea3
AirCraft009 <coconraupe@gmail.com>
2025-11-04 23:40:14 +0100

    bug fix

--- x64/build/buildScript.ps1
+++ x64/build/buildScript.ps1
@@ -1,6 +1,11 @@
 # make sure errors stop
 $ErrorActionPreference = "Stop"
 
+$root = Resolve-Path ../
+
+nasm "$root/boot/MBR.asm" -o "MBR.bin"
+nasm "$root/boot/Bootloader.asm" -o "$root/build/Bootloader.bin"
+
 # read both binaries as byte arrays (no text conversion!)
 [byte[]]$boot  = [System.IO.File]::ReadAllBytes("x64/build/MBR.bin")
 [byte[]]$stage = [System.IO.File]::ReadAllBytes("x64/build/Bootloader.bin")
@@ -16,5 +21,8 @@ $fs = [System.IO.File]::Open("x64/build/hdd.img", [System.IO.FileMode]::Create)
 $fs.SetLength($size)
 $fs.Write($combined, 0, $combined.Length)
 $fs.Close()
+$hdd = [System.IO.File]::ReadAllBytes("x64/build/hdd.img")
+$hdd[0x1BE] = 0x80
+[System.IO.File]::WriteAllBytes("x64/build/hdd.img", $hdd)
 
 Write-Host "Created 10MB hdd.img with bootloader + stage2"
